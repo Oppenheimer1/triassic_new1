b37088e241d3a4db3e9c70173826656b2099bc1d
LARS BERG <larsgbr@gmail.com>
2016-08-07 22:17:47 -0500

    Created Ruby on Rails CMS

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+TriassicNew1::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TriassicNew1::Application.config.secret_token = 'e634b9e21d93039576695181f882d9d0924e6cf5b7d1d80add981889bcadeb4af2fe7169419c5fd1b0626546a188951f8d6d554722f6985997da50fcb4504317'
